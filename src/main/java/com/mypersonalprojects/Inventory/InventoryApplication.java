f2818d8222bae40160f59530f600391e5a71c6c8
Shun <hirashun1224@gmail.com>
2025-06-18 19:51:41 +0900

    inital

---
+++ src/main/java/com/mypersonalprojects/Inventory/InventoryApplication.java
@@ -0,0 +1,15 @@
+package com.mypersonalprojects.Inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryApplication {
+
+//	4
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryApplication.class, args);
+	}
+
+}
